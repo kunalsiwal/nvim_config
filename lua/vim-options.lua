047d2c49deed548a1eee0fc389ebe20293ecdc3e
kunal_siwal <kunalissiwal@gmail.com>
2024-01-06 15:56:51 +0530

    completed the config for nvim

---
+++ lua/vim-options.lua
@@ -0,0 +1,9 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=2")
+vim.cmd("highlight Normal guibg=none")
+vim.cmd("highlight NonText guibg=none")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.g.mapleader=" "
